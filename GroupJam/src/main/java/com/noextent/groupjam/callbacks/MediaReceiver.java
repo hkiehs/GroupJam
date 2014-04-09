4de8de761020c0fae5350d6e7e5b0e11f0075af0
Sheikh Muneeb <muneeb118@gmail.com>
2014-04-09 08:43:16 +0500

    added media receiver

---
+++ GroupJam/src/main/java/com/noextent/groupjam/callbacks/MediaReceiver.java
@@ -0,0 +1,7 @@
+package com.noextent.groupjam.callbacks;
+
+import com.noextent.groupjam.model.ParseMedia;
+
+public interface MediaReceiver {
+        public void onMediaReceived(ParseMedia parseMedia);
+    }
